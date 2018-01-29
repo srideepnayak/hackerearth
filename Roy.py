88eb212c52f74a58461ba43f8870b4b31775f24a
Srideep Nayak <nayak.srideep@gmail.com>
2018-01-29 11:08:45 +0530

    Update Roy.py

--- Roy.py
+++ Roy.py
@@ -1,29 +1,12 @@
 L = int(input())
 N = int(input())
-dimension=[]
 
-while N>0:
-    s = input()
-    dimension.append(s)
-    N-=1
-
-print(dimension)
-
-def check(L,W,H):
-    if W < L or H < L:
-        print('UPLOAD ANOTHER')
-    elif W >= L or H >= L:
-        if W == H:
-            print('ACCEPTED')
-        else:
-            print('CROP IT')
+dimension = [input() for i in range(0,N)]
 
 for item in dimension:
-    i = item.split(' ')
-    W = i[0]
-    H = i[1]
-    check(L,int(W),int(H))
-
-
-
-
+    W = int(item.split(' ')[0])
+    H = int(item.split(' ')[1])
+    if W < L or H < L: print('UPLOAD ANOTHER')
+    elif W == H:print('ACCEPTED')
+    else: print('CROP IT')
+   
